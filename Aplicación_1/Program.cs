479b36473660f4ed78eda685e7965e04ae31b995
Joaquin <joaquinquispe2022@gmail.com>
2025-04-09 08:33:20 -0500

    confirmar

---
+++ Aplicación_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicación_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicio
+            Console.WriteLine("Hola C#");
+                          
+                          
+
+
+
+
+            //Final
+            Console.ReadKey();
+        }
+    }
+}
